a53ef13c7c0da4248a53f68c05d7f962de715f65
damthoai002@gmail.com <damthoai002@gmail.com@4e9c118f-51d1-df41-b619-755be95d7236>
2013-01-29 13:38:57 +0000

    commit

--- trunk/LastestNew.aspx.cs
+++ trunk/LastestNew.aspx.cs
@@ -21,35 +21,36 @@ public partial class Service : System.Web.UI.Page
             }
 
         }
+
+        NewsViewModel = new NewsViewModel();
     }
+
+    NewsViewModel NewsViewModel;
+
+
+
+
     public void GetData()
     {
         GetData("");
     }
+
+
     public void GetData(string Id)
     {
         if (Id != "")
         {
-            List<NewsModel> lst = new List<NewsModel>();
-            for (int i = 0; i < 10; i++)
-            {
-                NewsModel n = new NewsModel("test" + Id + i.ToString(), "test" + Id + i.ToString(), "test" + Id + i.ToString(), "test" + Id + i.ToString(), 'a', "images/3page_img1.jpg", "test" + Id + i.ToString(), "test" + Id + i.ToString(), "test" + Id + i.ToString(), "test" + Id + i.ToString(), "test" + Id + i.ToString(), "test" + Id + i.ToString(), "test" + Id + i.ToString());
-                lst.Add(n);
-            }
+
+            List<NewsModel> lst = NewsViewModel.SelectAll(Id);
             lstViewNew.DataSource = lst;
             lstViewNew.DataBind();
         }
         else
         {
-            List<NewsModel> lst = new List<NewsModel>();
-            for (int i = 0; i < 10; i++)
-            {
-                NewsModel n = new NewsModel("test" + i.ToString(), "test" + i.ToString(), "test" + i.ToString(), "test" + i.ToString(), 'a', "images/3page_img1.jpg", "test" + i.ToString(), "test" + i.ToString(), "test" + i.ToString(), "test" + i.ToString(), "test" + i.ToString(), "test" + i.ToString(), "test" + i.ToString());
-                lst.Add(n);
-            }
+            List<NewsModel> lst = NewsViewModel.SelectAll();
             lstViewNew.DataSource = lst;
             lstViewNew.DataBind();
         }
-       
+
     }
 }
